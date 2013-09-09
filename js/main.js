960d32eb4a61dd5c1aa8a35dce66f971c00d0f19
Nick Robinson <grobins2@gmail.com>
2013-09-09 03:49:41 -0400

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 var tabs = {};
-var tabs_keys = [];
-
 
 $('#saveButton').click(function(){
     saveCurrentTabs();
@@ -9,6 +7,33 @@ $('#saveButton').click(function(){
 $('#clearTabs').click(function(){
     closeAllTabs();
 });
+
+$('#myModal').on('hidden.bs.modal', function () {
+    $(this).removeData('bs.modal');
+});
+
+$(document.body).on('click', '#displayModal', function(){
+    $('#myModal').modal('show');
+    $('.modal-body').empty();
+    var key = $(this).context.name;
+    var size = Object.keys(tabs[key].data).length;
+    $('.modal-body').append(
+        '<table class="table-bordered table-condenced">'+
+        '<tbody id="tabcontent">'+
+        '</tbody>'+
+        '</table>'
+        );
+
+    for (var i=0; i < size; i++){
+        $('#tabcontent').append(
+            '<tr>'+
+            '<td><img src="'+tabs[key].data[i].favIconUrl+'"</img></td>'+
+            '<td>'+tabs[key].data[i].title+'</td>'+
+            '</tr>'
+            );
+    }
+});
+
 
 len = function(obj){
     var size = 0;
@@ -20,7 +45,7 @@ len = function(obj){
 
 saveCurrentTabs = function(){
     var temp_tabs = {};
-    var datetime = moment().format("MMMM Do YYYY, h:mm:ss a");
+    var key = moment().format("MMMM Do YYYY, h:mm:ss a");
 
     chrome.tabs.query({}, function(tab_array){
         for(var i = 0; i < tab_array.length; i++){
@@ -31,10 +56,8 @@ saveCurrentTabs = function(){
                     "favIconUrl":tab_array[i].favIconUrl
                 };
         }
-        tabs[datetime] = {"data": temp_tabs};
-        tabs_keys[tabs_keys.length] = datetime;
-        chrome.storage.local.set({'tabs':tabs}, function(){console.log('saved tabs');});
-        chrome.storage.local.set({'tabs_keys': tabs_keys}, function(){console.log('saved keys');});
+        tabs[key] = {"data": temp_tabs};
+        chrome.storage.local.set({'tabs':tabs}, function(){console.log(key + ' saved');});
         displaySavedTabs();
     });
 };
@@ -50,62 +73,66 @@ closeAllTabs = function(){
 };
 
 displaySavedTabs = function(){
+    var keys = Object.keys(tabs);
     $('#tabview').empty();
-    for (var i=tabs_keys.length-1; i >= 0; i--){
-        $('#tabview').append("<tr>"
-                + "<td class='tabname'>" + tabs_keys[i] + "</td>"
-                + "<td><div class='btn-group btn-group-xs'>"
-                + "<button data-toggle='modal' href='#myModal' name='"+tabs_keys[i]+"' id='displayModal' class='btn btn-default btn-xs name='preview'><span class='glyphicon glyphicon-eye-open'> Preview</span></button>"
-                + "<button type='button' class='btn btn-default btn-xs' name='restore' value='"+tabs_keys[i]+"'><span class='glyphicon glyphicon-folder-open'> Restore</span></button>"
-                + "<button type='button' class='btn btn-danger btn-xs' name='remove' value='"+tabs_keys[i]+"'><span class='glyphicon glyphicon-trash'> Delete</span></button></div></td>"
-                + "</tr>");
+    for (var i=keys.length-1; i >= 0; i--){
+        $('#tabview').append(
+                "<tr>"+
+                "<td class='tabname'>" + keys[i] + "</td>"+
+                "<td><div class='btn-group btn-group-xs'>"+
+                "<button data-toggle='modal' data-target='myModal' name='"+keys[i]+"' id='displayModal' class='btn btn-default btn-xs name='preview'><span class='glyphicon glyphicon-eye-open'> Preview</span></button>"+
+                "<button type='button' class='btn btn-default btn-xs' name='restore' value='"+keys[i]+"'><span class='glyphicon glyphicon-folder-open'> Restore</span></button>"+
+                "<button type='button' class='btn btn-danger btn-xs' name='remove' value='"+keys[i]+"'><span class='glyphicon glyphicon-trash'> Delete</span></button></div></td>"+
+                "</tr>"
+                );
     }
 
     $('.tabname').hover(function(){
         $(this).toggleClass('active');
     });
+
     $('button').click(function(){
-        var date = $(this).context.value;
-        if($(this).context.name.toString() == "remove"){
-            removeFromHistory(date);
-        } else if($(this).context.name.toString() == "restore"){
-            restoreTabs(date);
+        var key = $(this).context.value;
+        if($(this).context.name == "remove"){
+            removeFromHistory(key);
+        } else if($(this).context.name == "restore"){
+            restoreTabs(key);
         }
     });
 
-    $('#displayModal').show(function(){
-        $('.modal-body').empty();
-        var date = $(this).context.name;
-        for (var i=0; i < len(tabs[date].data); i++){
-            $('.modal-body').append('<li>'+tabs[date].data[i].title+'</li>');
-        }
-    });
+//    $(document.body).on('click', '#displayModal', function(){
+//        $('#myModal').modal('show');
+//        $('.modal-body').empty();
+//        var key = $(this).context.name;
+//        var size = Object.keys(tabs[key].data).length;
+//
+//        for (var i=0; i < size; i++){
+//            $('.modal-body').append('<li>'+tabs[key].data[i].title+'</li>');
+//        }
+//    });
 };
 
 loadTabHistory = function() {
-    chrome.storage.local.get(['tabs','tabs_keys'],function(all){
-        if(all.tabs && all.tabs_keys){
-            tabs = all.tabs;
-            tabs_keys = all.tabs_keys;
+    chrome.storage.local.get('tabs',function(stored_tabs){
+        if(stored_tabs.tabs){
+            tabs = stored_tabs.tabs;
         }
         displaySavedTabs();
     });
 };
 
-restoreTabs = function(date) {
-    for(var i=0; i < len(tabs[date].data); i++){
-        chrome.tabs.create({"url":tabs[date].data[i].url});
+restoreTabs = function(key) {
+    var size = Object.keys(tabs[key].data).length;
+    for(var i=0; i < size; i++){
+        chrome.tabs.create({"url":tabs[key].data[i].url});
     }
 };
 
-removeFromHistory = function(d) {
-    var restoreDate = d.toString();
-    var index = tabs_keys.indexOf(d);
-    delete tabs[d];
-    tabs_keys.splice(index, 1);
+removeFromHistory = function(key) {
+    delete tabs[key];
     chrome.storage.local.set({'tabs':tabs}, function(){console.log('saved tabs');});
-    chrome.storage.local.set({'tabs_keys': tabs_keys}, function(){console.log('saved keys');});
     displaySavedTabs();
 };
+
 
 loadTabHistory();
